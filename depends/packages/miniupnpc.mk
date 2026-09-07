750695a7cd99ccddb91a1d088200f551b9ef8cb0
tryiou <tryou@tryou.com>
2026-09-07 11:36:50 +0200

    deps: bump zlib/libevent/miniupnpc (CVE, compat)
    - zlib 1.2.11 -> 1.3.1 (CVE-2023-6992, CVE-2022-37434) https://github.com/madler/zlib/releases/download/v1.3.1
    - libevent 2.1.11 -> 2.1.12-stable (https://github.com/libevent/libevent/releases/download/release-2.1.12-stable)
    - miniupnpc 2.0.20180203 -> 2.2.2 (https://miniupnp.tuxfamily.org/files, was free.fr)
    
    Verified curl+sha256 for each; qrencode 4.1.1 deferred (needs CMake migration, fukuchi 404)

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
-$(package)_version=2.0.20180203
-$(package)_download_path=http://miniupnp.free.fr/files
+$(package)_version=2.2.2
+$(package)_download_path=https://miniupnp.tuxfamily.org/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=90dda8c7563ca6cd4a83e23b3c66dbbea89603a1675bfdb852897c2c9cc220b7
+$(package)_sha256_hash=888fb0976ba61518276fe1eda988589c700a3f2a69d71089260d75562afd3687
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
